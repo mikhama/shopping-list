2149f391edceec74ca9b8b24bb2fcdda57923b2a
Dzmitry Tsebruk <mikhama@tut.by>
2020-08-03 22:26:28 +0300

    feat: add PWA skeleton with svelte

---
+++ src/components/GroceryItem.svelte
@@ -0,0 +1,22 @@
+<script>
+  import { groceriesStore } from '../stores';
+
+  export let groceryItem;
+
+  function deleteGroceryItem() {
+    groceriesStore.update((groceries) => {
+      const deletedId = groceries.findIndex(({ id }) => groceryItem.id === id);
+      return [...groceries.slice(0, deletedId), ...groceries.slice(deletedId + 1)];
+    });
+  }
+</script>
+
+<span on:click={deleteGroceryItem}>{groceryItem.text}</span>
+
+<style>
+  span {
+    padding: 10px;
+    font-size: 22px;
+    background-color: rgb(255, 255, 169);
+  }
+</style>
